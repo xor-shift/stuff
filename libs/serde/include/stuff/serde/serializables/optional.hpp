110e8426a6c093a24b28fdc632554a0de138dc79
xor-shift <uygarb@protonmail.com>
2023-05-12 16:22:17 +0300

    forgot a file, oops

---
+++ libs/serde/include/stuff/serde/serializables/optional.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stuff/serde/concepts.hpp>
+
+#include <optional>
+
+namespace std {
+
+template<typename Serializer, typename T>
+constexpr auto _stf_adl_serialize(Serializer&& serializer, optional<T> const& v)  //
+  -> stf::expected<typename std::remove_cvref_t<Serializer>::value_type, typename std::remove_cvref_t<Serializer>::error_type> {
+    if ((bool)v) {
+        return serializer.serialize_some(*v);
+    } else {
+        return serializer.template serialize_none<T>();
+    }
+}
+
+template<typename Serializer, typename T>
+constexpr auto _stf_adl_serialize(Serializer&& serializer, optional<T>&& v)  //
+  -> stf::expected<typename std::remove_cvref_t<Serializer>::value_type, typename std::remove_cvref_t<Serializer>::error_type> {
+    if ((bool)v) {
+        return serializer.serialize_some(std::move(*std::move(v)));
+    } else {
+        return serializer.template serialize_none<T>();
+    }
+}
+
+}  // namespace std
